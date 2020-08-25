fdcfeba036c93535ebc749b8865773819d094def
Krishnaarpit12 <67108304+Krishnaarpit12@users.noreply.github.com>
2020-08-25 16:48:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var car, wall;
+var c2, c3;
 var speed, weight;
 
 function setup() {
@@ -6,6 +7,23 @@ function setup() {
 
   speed = random(55, 90);
   weight = random(400, 1500);
+
+  car = createSprite(50, 200, 50, 50);
+  car.shapeColor = color(0);
+  car.velocityX = speed;
+
+  c2 = createSprite(50, 100, 50, 50);
+  c2.shapeColor = color(0);
+  c2.velocityX = speed;
+
+  c3 = createSprite(50, 300, 50, 50);
+  c3.shapeColor = color(0);
+  c3.velocityX = speed;
+
+  wall = createSprite(1400, 200, 60, 300);
+  wall.shapeColor =color(80, 80, 80);
+
+  
   
 }
 
@@ -32,13 +50,46 @@ function draw() {
     }
     
   }
+  if(wall.x - c2.x < (c2.width + wall.width)/2)
+  {
+    c2.velocityX = 0;
+    var deformation = 0.5 * weight * speed* speed/22509;
+    if(deformation>180)
+    {
+      c2.shapeColor = color(255, 0, 0);
+    }
 
-  car = createSprite(50, 200, 50, 50);
-  car.velocityX = speed;
+    if(deformation<180 && deformation>100)
+    {
+      c2.shapeColor = color(230, 230, 0);
+    }
 
-  wall = createSprite(1400, 200, 60, height/2);
-  wall.shapeColor =color(80, 80, 80);
+    if(deformation<100)
+    {
+      c2.shapeColor = color(0, 255, 0);
+    }
+    
+  }
+  if(wall.x - c3.x < (c3.width + wall.width)/2)
+  {
+    c3.velocityX = 0;
+    var deformation = 0.5 * weight * speed* speed/22509;
+    if(deformation>180)
+    {
+      c3.shapeColor = color(255, 0, 0);
+    }
 
+    if(deformation<180 && deformation>100)
+    {
+      c3.shapeColor = color(230, 230, 0);
+    }
+
+    if(deformation<100)
+    {
+      c3.shapeColor = color(0, 255, 0);
+    }
+    
+  }
 
   drawSprites();
 }
